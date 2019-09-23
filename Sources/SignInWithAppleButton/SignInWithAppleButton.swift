3df8edf707b25bd0030ce7066bc19c27b5f8145f
Thibaut Mottet <thibaut.mottet@gmai.com>
2019-09-23 12:19:51 +0200

    Fix

--- Sources/SignInWithAppleButton/SignInWithAppleButton.swift
+++ Sources/SignInWithAppleButton/SignInWithAppleButton.swift
@@ -1,3 +1,34 @@
-struct SignInWithAppleButton {
-    var text = "Hello, World!"
+@available(iOS 13.0, *)
+public struct SignInWithAppleButton : View {
+    var type : ASAuthorizationAppleIDButton.ButtonType = .default
+    var style : ASAuthorizationAppleIDButton.Style? = nil
+    
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+
+    var body: some View {
+       AppleSignInButtonView(type: type,
+                             style: style == nil ? self.colorScheme == .dark ? .white : .black : style!)
+           .frame(height: 50, alignment: .center).padding(.all)
+    }
+}
+
+struct AppleSignInButtonView: UIViewRepresentable {
+    var type : ASAuthorizationAppleIDButton.ButtonType = .default
+    var style : ASAuthorizationAppleIDButton.Style = .white
+    
+    func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
+        return  ASAuthorizationAppleIDButton(authorizationButtonType: type, authorizationButtonStyle: style)
+    }
+    
+    func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: UIViewRepresentableContext<AppleSignInButtonView>) {}
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SignInWithAppleButton().environment(\.colorScheme, .dark)
+            SignInWithAppleButton()
+        }
+    }
 }
